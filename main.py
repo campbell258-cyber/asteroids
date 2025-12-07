5469afc91ec5473b3d0a4c4a05daf8704ca9f488
campbell258-cyber <campbell258@gmail.com>
2025-12-07 13:28:07 -0500

    CH3.L2

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
-
+from player import Player
 
 def main():
     pygame.init()
@@ -12,15 +12,18 @@ def main():
     black = (0, 0, 0)
     clock = pygame.time.Clock()
     dt = 0
+    player = Player(x = SCREEN_WIDTH / 2, y = SCREEN_HEIGHT / 2)
     while True:
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill(black)
+        player.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60)
         dt = dt/1000
+       
 
 if __name__ == "__main__":
     main()
