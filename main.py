293317eb355022d68eb1085958b6c8bd58fe344f
campbell258-cyber <campbell258@gmail.com>
2025-12-07 12:36:06 -0500

    CH2.L4

--- main.py
+++ main.py
@@ -10,6 +10,8 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     black = (0, 0, 0)
+    clock = pygame.time.Clock()
+    dt = 0
     while True:
         log_state()
         for event in pygame.event.get():
@@ -17,6 +19,8 @@ def main():
                 return
         screen.fill(black)
         pygame.display.flip()
+        dt = clock.tick(60)
+        dt = dt/1000
 
 if __name__ == "__main__":
     main()
